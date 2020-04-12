f95620c4d29aeb7ce3e9c1d8d8517c262e53fd31
namduong887 <nam.duong.887@gmail.com>
2020-04-12 15:01:48 +1000

    Fresh start of the front end project

---
+++ media-client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello React,Webpack 4 & Babel 7!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+console.log('hell')
---
+++ media-client/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ],
+    output: {
+        filename: '[name].[contenthash].js'
+    }
+};
