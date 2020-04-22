a81b72df931c663ba25c3cb19f7df22832dc482f
namduong887 <nam.duong.887@gmail.com>
2020-04-22 22:49:45 +1000

    no message

--- media-server/MediaServer.WebRtc.Native/rtp_receiver.cpp
+++ media-server/MediaServer.WebRtc.Native/rtp_receiver.cpp
@@ -5,7 +5,7 @@
 Wrappers::RtpReceiver::RtpReceiver(rtc::scoped_refptr<webrtc::RtpReceiverInterface> &&rtp_receiver)
     : _rtp_receiver(rtp_receiver)
 {
-    ;
+    
 }
 
 webrtc::RtpReceiverInterface *Wrappers::RtpReceiver::GetRtpReceiverInterface() const
