bf96d039ab1203f878f87fa71db9337b53203e2c
Nam Duong <nam.duong.887@gmail.com>
2020-05-10 18:50:58 +1000

    The client now declare tracks upfront, this simplifies the server side code, and avoid unnecessary re-negotation

--- media-client/src/net/peer-connection-controller.js
+++ media-client/src/net/peer-connection-controller.js
@@ -20,56 +20,14 @@ function getFirstTrackOrNull(stream, trackKind) {
 }
 
 /**
- * @param {RTCPeerConnection} peerConnection 
- * @param {MediaStreamTrack} prevTrack 
- * @param {MediaStreamTrack} nextTrack 
- */
-function replaceTrack(peerConnection, prevTrack, nextTrack) {
-    if (prevTrack) {
-        var prevAudioTransceiver = peerConnection
-            .getTransceivers()
-            .filter(trans => trans.sender.track == prevTrack)
-            .shift();
-        if (!prevAudioTransceiver) {
-            FatalErrorHandler.failFast(`Failed finding transceiver for track ${prevTrack.id} ${prevTrack.kind}`);
-        }
-        prevAudioTransceiver.sender.replaceTrack(nextTrack);
-    }
-    else {
-        peerConnection.addTrack(nextTrack);
-    }
-}
-
-/**
- * Should be called everytime the PeerConnection restarts,
- * or local media stream change, so we can re-associate them.
+ * Replace transceiver's track
  * 
- * @param {MediaStream} oldStream 
- * @param {MediaStream} newStream 
+ * @param {RTCRtpTransceiver} transceiver 
+ * @param {MediaStream} stream 
+ * @param {String} kind 
  */
-function setStream(peerConnection, oldStream, newStream) {
-    if (peerConnection == null) {
-        throw 'SetStreamCalledWhilePeerConnectionIsNull';
-    }
-    // PeerConnection exists but its stream has not changed?
-    // Early exit.
-    if (oldStream == newStream) {
-        logger.debug('Stream not changed, ignoring the change.');
-        return;
-    }
-    logger.info('Local media stream changed, updating PeerConnection..');
-
-    // If there is existing stream, we will REPLACE transceiver's tracks
-    var prevAudioTrack = getFirstTrackOrNull(oldStream, 'audio');
-    var prevVideoTrack = getFirstTrackOrNull(oldStream, 'video');
-    var nextAudioTrack = getFirstTrackOrNull(newStream, 'audio');
-    var nextVideoTrack = getFirstTrackOrNull(newStream, 'video');
-
-    replaceTrack(peerConnection, prevAudioTrack, nextAudioTrack);
-    replaceTrack(peerConnection, prevVideoTrack, nextVideoTrack);
-
-    logger.debug('New local audio track has been set', nextAudioTrack);
-    logger.debug('New local video track has been set', nextVideoTrack);
+function replaceTrack(transceiver, stream, kind) {
+    transceiver.sender.replaceTrack(getFirstTrackOrNull(stream, kind));
 }
 
 export default class PeerConnectionController extends WebSocketMessageHandler {
@@ -82,6 +40,10 @@ export default class PeerConnectionController extends WebSocketMessageHandler {
     _currentOfferer = null;
     /** @type {MediaStream} */
     _localStream = null;
+    /** @type {RTCRtpTransceiver} */
+    _audioTransceiver = null;
+    /** @type {RTCRtpTransceiver} */
+    _videoTransceiver = null;
 
     /**
      * @return {MediaStream}
@@ -92,19 +54,9 @@ export default class PeerConnectionController extends WebSocketMessageHandler {
      * @param {MediaStream} value
      */
     set localStream(value) {
-        var oldStream = this._localStream;
         this._localStream = value;
         if(this._peerConnection)
-        {
-            if(this._currentOfferer && !this._currentOfferer.isCompleted) {
-                // There's some negotiation underway
-                // Wait for negotiation complete before changing the stream again,
-                // because changing the stream causes re-negotiation
-                this._currentOfferer.onComplete(() => setStream(this._peerConnection, oldStream, value));
-            } else {
-                setStream(this._peerConnection, oldStream, value);    
-            }
-        }
+            this._replaceTracksFromStream(value);
     }
 
     constructor(webSocketClient) {
@@ -118,20 +70,17 @@ export default class PeerConnectionController extends WebSocketMessageHandler {
     _handleRoomJoined() {
         // Everytime we join a room, restart the PeerConnection
         // This is due to the life time of a PeerConnection 
-        // always associated with the life time of the WebSocket connection
+        // always associated with the life time of the WebSocket connection.
+        // 
+        // The 'negotiationneeded' event will be triggered (due to
+        // transceivers are created) and the negotiation process starts
+        // immediately.
         this._restart();
-
-        // Set the track if we have at least one track,
-        // this will then trigger "negotiation required", therefore starts the offer process.
-        if (this._localStream && this._localStream.getTracks().length > 0) {
-            setStream(this._peerConnection, null, this._localStream);
-        }
-        // Else, if we have no track, manually start the offer process.
-        // Otherwise PeerConnection will never established.
-        else {
-            logger.warn('No local media stream, starting offer process manually..');
-            this._startOfferProcess();
-        }
+        
+        // At this point, PeerConnection is created and the negotation process
+        // will start. We set tracks for the transceivers: (this won't cause 
+        // re-negotiation)
+        this._replaceTracksFromStream(this._localStream);
     }
 
     _restart() {
@@ -155,7 +104,14 @@ export default class PeerConnectionController extends WebSocketMessageHandler {
                 this._onRemoteStream(e);
             }
         });
+        this._audioTransceiver = this._peerConnection.addTransceiver('audio');
+        this._videoTransceiver = this._peerConnection.addTransceiver('video');
         logger.info('PeerConnection created');
+    }
+
+    _replaceTracksFromStream(stream) {
+        replaceTrack(this._audioTransceiver, stream, 'audio');
+        replaceTrack(this._videoTransceiver, stream, 'video');
     }
 
     _startOfferProcess() {
